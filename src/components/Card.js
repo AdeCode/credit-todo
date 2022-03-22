e18adf26aaa43ecd4120c1eef31412cb24cc9a48
adecode <agorohabeeb@gmail.com>
2022-03-22 15:18:14 +0100

    added dran and drop feature to the card item list

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,29 +3,17 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import Checkbox from '@mui/material/Checkbox';
 import CloseIcon from '@mui/icons-material/Close';
-import cross from '../images/icon-cross.svg'
-import favicon from '../images/favicon-32x32.png'
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
-
 
 
 export default function Card({title,deleteTask,markComplete,status}) {
   return (
-    <div className='card'>      
-        {/* <Checkbox
-            checked={!status} 
-            onChange={markComplete}
-            icon={<CheckCircleOutlineIcon />}
-            checkedIcon={<CheckCircleIcon />}
-        
-        /> */}
-
+    <div className='card-item'>              
         {status===true ? 
             <RadioButtonUncheckedIcon
                 checked={!status} 
                 onClick={markComplete}
                 icon={<CheckCircleOutlineIcon />}
-                checkedIcon={<CheckCircleIcon />}
                 style={{ marginLeft:'10px' }}
                 className='circle'
             />
@@ -41,13 +29,8 @@ export default function Card({title,deleteTask,markComplete,status}) {
          
         <span className={status===false ? 'completed title' : 'title'}>{title}</span>
         <span className='close'>
-            <img src={cross} onClick={() => deleteTask()}/>
-        {/* <CloseIcon
-            onClick={() => deleteTask()}
-        /> */}
-        </span>
-        
-        {/* <button onClick={() => deleteTask()}>Delete</button> */}
+            <CloseIcon onClick={() => deleteTask()} />
+        </span>        
     </div>
   )
 }
